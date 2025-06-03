4e33f1e86b994f1de8baae43a2b3cad636eb5de5
leonard.lei <1287279970@qq.com>
2025-06-04 00:52:37 +0800

    🐞 fix(AiMcp):  dependence

--- waas-mcp/src/main/java/me/flyray/bsin/facade/mcp/config/AiMcpToolsConfig.java
+++ waas-mcp/src/main/java/me/flyray/bsin/facade/mcp/config/AiMcpToolsConfig.java
@@ -1,9 +1,9 @@
 package me.flyray.bsin.facade.mcp.config;
 
 import me.flyray.bsin.facade.mcp.OpenMeteoService;
+import org.springframework.ai.support.ToolCallbacks;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.ai.tool.ToolCallbackProvider;
-import org.springframework.ai.tool.ToolCallbacks;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
